dceebafd5a6a177d97246ec1306d63853d8bbdb7
Andrey Kovalenko <ak@taucode.com>
2022-08-08 02:22:35 +0300

    Updated content for 1.5.0.

--- src/TauCode.Cqrs.NHibernate/TransactionalCommandHandlerDecorator.cs
+++ src/TauCode.Cqrs.NHibernate/TransactionalCommandHandlerDecorator.cs
@@ -1,39 +1,35 @@
 ﻿using NHibernate;
-using System;
 using System.Data;
-using System.Threading;
-using System.Threading.Tasks;
 using TauCode.Cqrs.Abstractions;
 using TauCode.Cqrs.Commands;
 
-namespace TauCode.Cqrs.NHibernate
+namespace TauCode.Cqrs.NHibernate;
+
+public class TransactionalCommandHandlerDecorator<TCommand> : ICommandHandler<TCommand> where TCommand : ICommand
 {
-    public class TransactionalCommandHandlerDecorator<TCommand> : ICommandHandler<TCommand> where TCommand : ICommand
+    protected readonly ICommandHandler<TCommand> CommandHandler;
+    private readonly ISession _session;
+
+    public TransactionalCommandHandlerDecorator(ICommandHandler<TCommand> commandHandler, ISession session)
     {
-        protected readonly ICommandHandler<TCommand> CommandHandler;
-        private readonly ISession _session;
+        this.CommandHandler = commandHandler ?? throw new ArgumentNullException(nameof(commandHandler));
+        _session = session ?? throw new ArgumentNullException(nameof(session));
+        _session.FlushMode = FlushMode.Commit;
+    }
 
-        public TransactionalCommandHandlerDecorator(ICommandHandler<TCommand> commandHandler, ISession session)
-        {
-            this.CommandHandler = commandHandler ?? throw new ArgumentNullException(nameof(commandHandler));
-            _session = session ?? throw new ArgumentNullException(nameof(session));
-            _session.FlushMode = FlushMode.Commit;
-        }
+    public virtual void Execute(TCommand command)
+    {
+        using var transaction = _session.BeginTransaction(IsolationLevel.ReadCommitted);
+        this.CommandHandler.Execute(command);
+        _session.Flush();
+        transaction.Commit();
+    }
 
-        public virtual void Execute(TCommand command)
-        {
-            using var transaction = _session.BeginTransaction(IsolationLevel.ReadCommitted);
-            this.CommandHandler.Execute(command);
-            _session.Flush();
-            transaction.Commit();
-        }
-
-        public async Task ExecuteAsync(TCommand command, CancellationToken cancellationToken)
-        {
-            using var transaction = _session.BeginTransaction(IsolationLevel.ReadCommitted);
-            await this.CommandHandler.ExecuteAsync(command, cancellationToken);
-            await _session.FlushAsync(cancellationToken);
-            await transaction.CommitAsync(cancellationToken);
-        }
+    public async Task ExecuteAsync(TCommand command, CancellationToken cancellationToken)
+    {
+        using var transaction = _session.BeginTransaction(IsolationLevel.ReadCommitted);
+        await this.CommandHandler.ExecuteAsync(command, cancellationToken);
+        await _session.FlushAsync(cancellationToken);
+        await transaction.CommitAsync(cancellationToken);
     }
 }
