d89cb1f2643829a9b3097fc0355fa777a52831aa
Andrey Kovalenko <ak@taucode.com>
2022-08-12 13:36:15 +0300

    Fixed batch file.

--- pre-deploy.cmd
+++ pre-deploy.cmd
@@ -3,7 +3,7 @@ dotnet restore
 dotnet build --configuration Debug
 dotnet build --configuration Release
 
-dotnet test -c Debug .\tests\TauCode.Cqrs.NHibernate.Tests\TauCode.Cqrs.NHibernate.Tests.csproj
-dotnet test -c Release .\tests\TauCode.Cqrs.NHibernate.Tests\TauCode.Cqrs.NHibernate.Tests.csproj
+dotnet test -c Debug .\test\TauCode.Cqrs.NHibernate.Tests\TauCode.Cqrs.NHibernate.Tests.csproj
+dotnet test -c Release .\test\TauCode.Cqrs.NHibernate.Tests\TauCode.Cqrs.NHibernate.Tests.csproj
 
 nuget pack nuget\TauCode.Cqrs.NHibernate.nuspec
